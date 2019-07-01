758c38c9b5259707e86952605817238fbe970337
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-07-01 15:32:03 +0530

    Create minimumvalueintherange.py

---
+++ minimumvalueintherange.py
@@ -0,0 +1,8 @@
+a,b=list(map(int,input().split()))
+c=list(map(int,input().split()))[:a]
+d=list(map(int,input().split()))[:b]
+e=list(map(int,input().split()))[:b]
+f=list(map(int,input().split()))[:b]
+print(min(c+d))
+print(min(c+e))
+print(min(c+f))
