8edaf934079f5c993a09162cb53fc2dc4ba46e07
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-07-12 11:03:58 +0530

    Update repeatingdigit.py

--- repeatingdigit.py
+++ repeatingdigit.py
@@ -1,13 +1,6 @@
 a=input()
-t=[]
-c=0
-for i in range(0,len(a)):
-  t.append(i)
-for i in t:
-  for j in range(1,len(a)):
-    if(i==j):
-      c+=1
-if(c>0):
-  print("yes")
-else:
+b=set(a)
+if(len(a)==len(b)):
   print("no")
+else:
+  print("yes")
