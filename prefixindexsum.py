f47d5c9b1a17b29993ec35b501ad89cefd895e97
kalais263 <42733833+kalais263@users.noreply.github.com>
2019-07-16 14:20:38 +0530

    Update prefixindexsum.py

--- prefixindexsum.py
+++ prefixindexsum.py
@@ -1,8 +1,9 @@
 a=int(input())
 b=list(map(int,input().split()))[:a]
-c=0
-t=[]
-for i in range(0,a):
-  c=c+b[i]
-  t.append(c)
-print(*t,end="")
+c=1
+for i in b:
+  c=c*i
+if(c%2==0):
+  print("even")
+else:
+  print("odd")
